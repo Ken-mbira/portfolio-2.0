adfdaf5878731d136b957ac8379d18f64f21051b
ken <ken.mbira@student.moringaschool.com>
2021-11-29 09:39:41 +0300

    add contact me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './components/landing/landing.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SkillsetComponent } from './components/skillset/skillset.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    SkillsetComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
