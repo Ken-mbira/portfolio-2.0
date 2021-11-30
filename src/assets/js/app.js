9b11a846d1d51d09168d7227f168178ee0972acf
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-11-30 13:37:35 +0300

    add nav links transition on page on

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
         $('.active').removeClass('active');
         $(this).addClass('active');
     })
+
+    $(window).scroll(function () {
+        $('.target').each(function (){
+            if($(window).scrollTop() >= $(this).offset().top){
+                var id = $(this).attr('id');
+                $('.active').removeClass('active');
+                $(".page-nav").each(function(){
+                    if ($(this)[0].href == `http://localhost:4200/#${id}`){
+                        $(this).addClass('active');
+                    }
+                })
+            }
+        })
+    })
 })
